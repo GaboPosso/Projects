a88b5bf475edf41cb3da738490c0bc4117d97801
GaboPosso <gabopos@gmail.com>
2022-08-21 23:55:32 -0500

    start twitter page project

---
+++ easy-setup/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+//Import components
+import Nav from './components/Nav';
+
+function App() {
+  return (
+    <div className="App">
+      <Nav />
+    </div>
+  );
+}
+
+export default App;
