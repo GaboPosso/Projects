eca34c36298f15fa8ee6301dd3bbd49ea3b24906
GaboPosso <gabopos@gmail.com>
2022-08-26 20:51:59 -0500

    Update user clock app React from scratch

---
+++ clock/src/index.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+class Clock extends React.Component {
+    constructor (props) {
+      super(props);
+      this.state = {
+        date: new Date()};
+    }
+  
+    componentDidMount() {
+      this.timerID = setInterval(
+        () => this.tick(),
+        1000
+      );
+    }
+  
+    componentWillUnmount() {
+      clearInterval(this.timerID);
+    }
+  
+    tick() {
+      this.setState({
+        date: new Date()
+      });
+    }
+    
+    render() {
+      return (
+        <div>
+          <h1>Hi! </h1>
+          <h2>It is {this.state.date.toLocaleTimeString()}. </h2>
+        </div>
+      );
+    }  
+  }
+  
+  const root = ReactDOM.createRoot(document.getElementById('root'));
+  root.render(<Clock />);
